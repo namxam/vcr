ba447ff1e1b71b63183ff0f3daa8acbdcd30358a
Max <max@railslove.com>
2016-11-11 11:45:28 +0100

    Remove log statement spamming the console

--- src/utils/parseLink.js
+++ src/utils/parseLink.js
@@ -52,7 +52,6 @@ export default function parseLink(url, {
   i: resultIndex,
   p: subParser,
 }) {
-  console.info('[parseLink]', { url, linkType, regEx, replacement, decodeType, resultIndex, matchGroup, subParser })
   if (resultIndex !== -1) { console.error('resultIndex was not -1') }
 
   return LinkType[linkType](url)
